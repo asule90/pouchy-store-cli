42948f98f746206aae47bcff4126c689cc0cd500
Asep Sulaeman <asepsulaeman90@gmail.com>
2019-08-24 13:50:51 +0700

    feat(init): start cli-app using vorpal

---
+++ cli.js
@@ -0,0 +1,18 @@
+const vorpal = require('vorpal')();
+
+vorpal
+  .command('say [words...]')
+  .option('-b, --backwards')
+  .option('-t, --twice')
+  .action(function (args, callback) {
+    let str = args.words.join(' ');
+    str = (args.options.backwards) ?
+      str.split('').reverse().join('') :
+      str+' '+str;
+    this.log(str);
+    callback();
+  }); 
+
+vorpal
+    .delimiter('pouchy-store-cli$')
+    .show();
