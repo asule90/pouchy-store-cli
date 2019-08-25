7424863070a7ddad0580e2e38f5c361f705f60d0
Asep Sulaeman <asepsulaeman90@gmail.com>
2019-08-25 22:04:49 +0700

    feat(main): edit, sync
    edit an existing task, sync command to upload local tasks
    
    style(main): semicolon
    
    using semicolon to every statements
    
    fix(sync): checkInternet
    
    fix no internet condition

--- cli.js
+++ cli.js
@@ -4,41 +4,71 @@ import todosStore from './store/todos.js';
 const exitHook = require('async-exit-hook');
 var mkdirp = require('mkdirp');
 var moment = require('moment');
-    
+
 mkdirp('db', function (err) {
   if (err) {
-    console.error(err)
+    vorpal.error(err);
     vorpal.exec("exit");
   }
 });
 
-console.log('Type `help` to show available command')
-console.log('Type `logout` to exit')
+vorpal.log('Type `help` to show available commands');
+vorpal.log('Type `logout` to exit');
 
 try {
   userStore.initialize();
 } catch (error) {
-  console.log(error)
-}
-let unsubUser;
-let unsubTodos;
-let stateUpdate;
-const saveState = function(){
-  stateUpdate = new Date()
+  vorpal.log(error);
 }
 
-/* componentWillUnmount() {
-  this.unsubUser();
-} */
+async function shutdown() {
+  vorpal.log('Exitting');
+  try {
+    await todosStore.deinitialize();
+  } catch (err) {
+    vorpal.log(err);
+  }
+  vorpal.log('Exitted');
+}
 
-const isLoggedIn = () => (!!userStore.data.email)
+let loggedIn = userStore.isInitialized || false
 
+let isLoggedIn = (cb) => {
+  if (!loggedIn) {
+    vorpal.log(vorpal.chalk.red('Please Login or type `help`'));
+    cb();
+    return false;
+  } else {
+    return true;
+  }
+};
+
+let isNumericArg = (args, cb) => {
+  if (isNaN(args.number)) {
+    vorpal.log(vorpal.chalk.red('Argument is not a number'));
+    cb();
+    return false;
+  } else {
+    return true;
+  }
+};
+let isTaskIndexExist = (index, cb) => {
+  if (Array.isArray(todosStore.data) && (index in todosStore.data)) {
+    return true;
+  } else {
+    vorpal.log(vorpal.chalk.red('Task is not recognized'));
+    cb();
+    return false;
+  }
+};
+
+// LOGIN
 vorpal
 .history('main-history')
 .command('login <email>')
 .description('Login with gmail account')
 .action(async function (args, callback) {
-  const _this = this
+  const _this = this;
   let email = args.email;
 
   if (!email.endsWith('@gmail.com')) {
@@ -49,212 +79,316 @@ vorpal
     let id = email;
     id = id.split('@').shift().replace(/\W/g, '');
 
-    // unsubUser = userStore.subscribe(saveState);
-    // _this.log(userStore.subscribers)
-
     await userStore.editSingle({
       id,
       email: email,
     });
 
     setTimeout(async () => {
+      loggedIn = true;
       _this.log("logged in as " + email);
       _this.log("user_id : " + userStore.data.id);
   
       if (!todosStore.isInitialized) {
         _this.log('popup initialize all offline data...');
-        todosStore.setName(userStore.data.id);
-        _this.log("store_name : " + todosStore.name);
-        await todosStore.initialize()
+        await todosStore.setName(userStore.data.id);
+        await todosStore.initialize();
         _this.log('popup done');
         // _this.log("store_name : " + todosStore.name);
+        callback();
+      } else {
+        callback();
       }
+
     }, 500)
   }
 
-  callback();
 });
 
-vorpal
-.history('main-history')
-.command('list')
-.description('List All Task, greyed out task is marked as done')
-.action(async function (args, callback) {
-  const _this = this
-
-  if (isLoggedIn()) {
-    // unsubTodos = todosStore.subscribe(saveState);
-    todosStore.data.forEach((todo, index) => {
-      process.stdout.write((parseInt(index)+1)+'. ')
-      if (todo.done) {
-        process.stdout.write(vorpal.chalk.grey(todo.text))
-      } else {
-        process.stdout.write(vorpal.chalk.green.bold(todo.text))
-      }
-      _this.log(', '+(todo.tags.map((v) => ('#'+v)))+vorpal.chalk.grey(' ('+moment(todo.createdAt).format('D/MMM/YYYY, H:mm:ss')+')'))
-    });
-
-  } else {
-    _this.log(vorpal.chalk.red('Please Login or type `help`'))
-  }
-
-  callback();
-});
-
+// LOGOUT
 vorpal
 .history('main-history')
 .command('logout')
 .description('Logout and exit cli')
 .action(async function () {
-  await shutdown()
+  vorpal.log('Logging out');
+  loggedIn = false;
+  await userStore.deleteSingle();
   vorpal.exec("exit");
 });
 
+// LIST
+vorpal
+.history('main-history')
+.command('list')
+.description('List All Task, greyed out task is marked as done')
+.action(async function (args, callback) {
+  const _this = this;
+
+  if (isLoggedIn(callback)) {
+    todosStore.data.forEach((todo, index) => {
+      process.stdout.write((parseInt(index)+1)+'. ')
+      if (todo.done) {
+        process.stdout.write(vorpal.chalk.grey(todo.text));
+      } else {
+        process.stdout.write(vorpal.chalk.green.bold(todo.text));
+      }
+      _this.log(', '+(todo.tags.map((v) => ('#'+v)))+vorpal.chalk.grey(' ('+moment(todo.createdAt).format('D/MMM/YYYY, H:mm:ss')+')'));
+    });
+  }
+  callback();
+});
+
+// ADD NEW TASK
 vorpal
 .history('main-history')
 .command('add')
-.description('Add a task')
+.description('Add a new task')
 .action(function (args, callback) {
-  const _this = this
-  // const index = parseInt(args.number) - 1
-  // const task = todosStore.data[index]
-  let task_text = ''
-  let tags = ''
-
-  _this.prompt({
-    type: 'input',
-    name: 'task_text',
-    message: 'New Task :'
-  }).then((result) => {
-    task_text = result.task_text
-    if (!task_text) {
-      _this.log(vorpal.chalk.red('Task is required'));
-      callback()
-    } else {
-      _this.prompt({
-        type: 'input',
-        name: 'tags_text',
-        message: 'Tags (comma delimited) :'
-      }).then(async (result) => {
-        tags = result.tags_text
+  if (isLoggedIn(callback)) {
+    const _this = this;
+    // const index = parseInt(args.number) - 1;
+    // const task = todosStore.data[index];
+    let task_text = '';
+    let tags = '';
   
-        if (!tags) {
-          _this.log(vorpal.chalk.red('Tags is required'));
-          callback()
-        } else {
-          tags = tags.split(',').map((v) => {
-            return v.trim();
-          })
-          
-          try {
-            await todosStore.addItem({
-              text: task_text,
-              tags: tags,
-              done: false
-            }, userStore.data)
-            _this.log(vorpal.chalk.green('New task has been added'));
-            callback()
-          } catch (error) {
-            _this.log(vorpal.chalk.red('Problem adding new task'));
-            _this.log(err);
-            callback()
+    _this.prompt({
+      type: 'input',
+      name: 'task_text',
+      message: 'New Task : '
+    }).then((result) => {
+      task_text = result.task_text;
+      if (!task_text) {
+        _this.log(vorpal.chalk.red('Task is required'));
+        callback();
+      } else {
+        _this.prompt({
+          type: 'input',
+          name: 'tags_text',
+          message: 'Tags (comma delimited) : '
+        }).then(async (result) => {
+          tags = result.tags_text;
+    
+          if (!tags) {
+            _this.log(vorpal.chalk.red('Tags is required'));
+            callback();
+          } else {
+            tags = tags.split(',').map((v) => {
+              return v.trim();
+            })
+            
+            try {
+              await todosStore.addItem({
+                text: task_text,
+                tags: tags,
+                done: false
+              }, userStore.data)
+              _this.log(vorpal.chalk.green('New task has been added'));
+              callback();
+            } catch (error) {
+              _this.log(vorpal.chalk.red('Problem adding new task'));
+              _this.log(err);
+              callback();
+            }
           }
-        }
-      })
-    }
-  })
+        })
+      }
+    })
+  }
 });
 
+// MARK TASK AS DONE
 vorpal
 .history('main-history')
 .command('done <number>')
 .description('Mark a task as done identified by its number')
 .action(async function (args, callback) {
-  const index = parseInt(args.number) - 1
-  const task = todosStore.data[index]
+  if (isLoggedIn(callback)) {
+    if (isNumericArg(args, callback)) {
+      const index = parseInt(args.number) - 1;
+      if (isTaskIndexExist(index, callback)) {
+        const task = todosStore.data[index];
+        
+        await todosStore.editItem(task._id, {
+          text: task.text,
+          tags: task.tags,
+          done: true
+        }, userStore.data)
+        vorpal.log(vorpal.chalk.green('Marked task #'+args.number+' as done'));
   
-  await todosStore.editItem(task._id, {
-    text: task.text,
-    tags: task.tags,
-    done: true
-  }, userStore.data)
-  vorpal.log(vorpal.chalk.green('Marked task #'+args.number+' as done'))
-
-  callback()
+        callback();
+      }
+    }
+  }
 });
 
+// MARK TASK AS UNDONE
 vorpal
 .history('main-history')
 .command('undone <number>')
 .description('Mark a task as undone identified by its number')
 .action(async function (args, callback) {
-  const index = parseInt(args.number) - 1
-  const task = todosStore.data[index]
-  
-  await todosStore.editItem(task._id, {
-    text: task.text,
-    tags: task.tags,
-    done: false
-  }, userStore.data)
-  vorpal.log(vorpal.chalk.green('Marked task #'+args.number+' as undone'))
+  if (isLoggedIn(callback)) {
+    if (isNumericArg(args, callback)) {
+      const index = parseInt(args.number) - 1;
+      if (isTaskIndexExist(index, callback)) {
+        const task = todosStore.data[index];
+        
+        await todosStore.editItem(task._id, {
+          text: task.text,
+          tags: task.tags,
+          done: false
+        }, userStore.data)
+        vorpal.log(vorpal.chalk.green('Marked task #'+args.number+' as undone'));
 
-  callback()
+        callback();
+      }
+    }
+  }
 });
 
+// DELETE A TASK
 vorpal
 .history('main-history')
 .command('delete <number>')
 .description('Delete a task identified by its number')
 .action(function (args, callback) {
-  const index = parseInt(args.number) - 1
-  const task = todosStore.data[index]
-  
-  try {
-    this.prompt({
-      type: 'confirm',
-      name: 'toBeDeleted',
-      message: 'You are going to delete task #'+args.number+', are you sure?',
-      default: false
-    }).then(async (result) => {
-      // vorpal.log(result.toBeDeleted)
-      // vorpal.log(task._id)
-      // vorpal.log(userStore.data)
-      
-      if (result.toBeDeleted) {
-        await todosStore.deleteItem(task._id, userStore.data);
-        vorpal.log(vorpal.chalk.green('Deleted task #'+args.number+''))
-        callback()
-      } else {
-        callback()
+  if (isLoggedIn(callback)) {
+    if (isNumericArg(args, callback)) {
+      const index = parseInt(args.number) - 1;
+      if (isTaskIndexExist(index, callback)) {
+        const task = todosStore.data[index];
+        
+        try {
+          this.prompt({
+            type: 'confirm',
+            name: 'toBeDeleted',
+            message: 'You are going to delete task #'+args.number+', are you sure?',
+            default: false
+          }).then(async (result) => {
+            // vorpal.log(result.toBeDeleted)
+            // vorpal.log(task._id)
+            // vorpal.log(userStore.data)
+            
+            if (result.toBeDeleted) {
+              await todosStore.deleteItem(task._id, userStore.data);
+              vorpal.log(vorpal.chalk.green('Deleted task #'+args.number+''));
+              callback();
+            } else {
+              callback();
+            }
+          })    
+        } catch (error) {
+          vorpal.log(error);
+          callback();
+        }
       }
-    })    
-  } catch (error) {
-    vorpal.log(error)
+    }
   }
 
 });
 
-
-/* process.on('exit', (code) => {
-  console.log(`About to exit with code: ${code}`);
-}); */
-
-async function shutdown() {
-  console.log('Logging out');
-  try {
-    await userStore.deleteSingle();
-    await todosStore.deinitialize();
-  } catch (err) {
-    console.log(err);
+// EDIT A TASK
+vorpal
+.history('main-history')
+.command('edit <number>')
+.description('Edit a task identified by its number')
+.action(function (args, callback) {
+  if (isLoggedIn(callback)) {
+    if (isNumericArg(args, callback)) {
+      const _this = this;
+      const index = parseInt(args.number) - 1;
+      if (isTaskIndexExist(index, callback)) {
+        const task = todosStore.data[index];
+        let task_text = '';
+        let tags = '';
+      
+        _this.prompt({
+          type: 'input',
+          name: 'task_text',
+          message: 'Edit Task '+args.number+' to : '
+        }).then((result) => {
+          task_text = result.task_text;
+          if (!task_text) {
+            _this.log(vorpal.chalk.red('Task is required'));
+            callback();
+          } else {
+            _this.prompt({
+              type: 'input',
+              name: 'tags_text',
+              message: 'Tags (comma delimited) : '
+            }).then(async (result) => {
+              tags = result.tags_text;
+        
+              if (!tags) {
+                _this.log(vorpal.chalk.red('Tags is required'));
+                callback();
+              } else {
+                tags = tags.split(',').map((v) => {
+                  return v.trim();
+                })
+                
+                try {
+                  await todosStore.editItem(task._id, {
+                    text: task_text,
+                    tags: tags,
+                  }, userStore.data);
+                  _this.log(vorpal.chalk.green('Edited the task'));
+                  callback();
+                } catch (error) {
+                  _this.log(vorpal.chalk.red('Problem editting the task'));
+                  _this.log(err);
+                  callback();
+                }
+              }
+            })
+          }
+        })
+      }
+    }
   }
-  console.log('Logged out');
-}
+
+});
+
+// SYNC TASKS
+vorpal
+.history('main-history')
+.command('sync')
+.description('Sync all task with the cloud')
+.action(function (args, callback) {
+  const _this = this;
+  if (isLoggedIn(callback)) {
+    const index = parseInt(args.number) - 1;
+    const task = todosStore.data[index];
+
+    const totalPendingData = todosStore.countUnuploadeds();
+
+    _this.prompt({
+      type: 'confirm',
+      name: 'toBeSynced',
+      message: totalPendingData+' local pending data, sync now?',
+      default: false
+    }).then(async (result) => {
+      if (result.toBeSynced) {
+        _this.log('syncing...');
+        await todosStore.upload().then(() => {
+          _this.log(vorpal.chalk.green('Synced all tasks'));
+        }).catch(err => {
+          _this.log(vorpal.chalk.red('Sync failed'));
+          _this.log(err.message);
+        })
+      } else {
+        _this.log('cancel');
+      }
+      callback();
+    })
+  }
+});
 
 vorpal.on('vorpal_exit', function onSigterm () {
   // start graceful shutdown here
-  shutdown()
+  shutdown();
 })
-
 
 vorpal
 .delimiter('pouchy-store-cli$')
