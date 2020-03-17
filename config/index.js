f1575bdceab5eddacc5c41fabc88a3a218017b91
Asep Sulaeman <10544636+asule90@users.noreply.github.com>
2020-03-17 15:08:36 +0700

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,7 +1,7 @@
 export default {
-  couchDBUrl: 'http://13.250.43.79:5984/', // 'http://localhost:5984'
+  couchDBUrl: 'http://localhost:5984',
   couchDBAuth: {
     username: 'admin',
-    password: 'iniadmin',
+    password: 'admin',
   },
 };
